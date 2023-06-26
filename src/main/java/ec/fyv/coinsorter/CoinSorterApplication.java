3dd365342c136754afecb9107f263bfde3438ce4
Fernando <wilmercaiza@gmail.com>
2023-06-26 14:05:44 -0500

    create project

---
+++ src/main/java/ec/fyv/coinsorter/CoinSorterApplication.java
@@ -0,0 +1,13 @@
+package ec.fyv.coinsorter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoinSorterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoinSorterApplication.class, args);
+	}
+
+}
